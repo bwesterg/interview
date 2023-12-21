db316d73f2e884dea3d31fa6d66e010d6d4dfa7a
bwesterg <benton.westergaard@gmail.com>
2023-12-21 14:59:53 -0700

    items rendering with basic styling applied. still need to implement search and refine styles.

--- src/static/app.js
+++ src/static/app.js
@@ -1,7 +1,37 @@
 window.addEventListener("DOMContentLoaded", setup);
 
+
 async function setup() {
 	// START HERE
 	// PRODUCTS CAN BE FETCHED USING: GET /products
 }
-console.log('hello');
+
+const baseURL = "http://localhost:3000"
+const productsURL = `${baseURL}/products`;
+let products;
+
+fetch(productsURL) 
+	.then(res => res.json())
+	.then(data => {
+		products = data;
+		renderProducts();
+	})
+	.catch(error => {
+		console.log('Error fetching products', error);
+	});
+
+function renderProducts() {
+	const container = document.getElementById('productContainer');
+	products.forEach(product => {
+		const card = document.createElement('div');
+		card.classList.add('product-card');
+		card.innerHTML = `
+			<img src="${product.images[0].src}" alt="${product.title}">
+			<h3>${product.title}</h3>
+			<p>$${(product.price / 100).toFixed(2)}</p>
+		`;
+		container.appendChild(card);
+		});
+	}
+	
+		
